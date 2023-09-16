a1c781c98b4ec3d68c84be50c1e0539e8651ad7c
Hanwen Zhang <hanwenzhang888@gmail.com>
2023-09-16 16:13:30 -0400

    changes push

--- js/main.js
+++ js/main.js
@@ -142,7 +142,7 @@ jQuery(document).ready(function( $ ) {
     autoplay: true,
     dots: true,
     loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+    responsive: { 0: { items: 1 }, 768: { items: 1 }, 900: { items: 3 }
     }
   });
 
